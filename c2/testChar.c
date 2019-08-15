3032069a290a1b711de07fbf268c78c83b563629
rollednetlib <32023525+rollednetlib@users.noreply.github.com>
2019-08-15 12:12:15 -0400

    Committed wrong file.

--- c2/testChar.c
+++ c2/testChar.c
@@ -2,18 +2,9 @@
 #include <string.h>
 
 int main(){
-	// Loop over input with the use of subscripts
 	char buf[50];
 	write(1,"What is the input string? ", 27);
 	read(0,buf, 50);
-	char *ps = buf;
-
- 	while(*ps == ' ')
-	{
-		printf("Please enter a valid name");
-		ps++;
-	}
-	printf("has %d characters\n", i);
 	
 	for(int  i = 0; i < strlen(buf); i++)
 	{
